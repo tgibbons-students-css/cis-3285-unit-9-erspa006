3765c8c436aaf71d1a7ffa56bce10b8107220a0c
Kyle Erspamer <kerspamer@css.edu>
2016-11-14 20:32:38 -0600

    Request  407 - all for passing in a url to create the tradeData.

---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using SingleResponsibilityPrinciple.Contracts;
+using System.Net;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class URLTradeDataProvider : ITradeDataProvider //full disclosure i think it works but i dont fully understand why
+    {
+        public URLTradeDataProvider(string url)
+        {
+            var client = new WebClient();
+            stream = client.OpenRead(url);
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<string>();
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
+
+        private readonly Stream stream;
+    }
+}
+
+
